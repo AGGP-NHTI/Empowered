1885cf027a6ccdf19c51258a6f1c9affe538b159
Toxic18 <49251120+Toxic18@users.noreply.github.com>
2019-04-17 16:39:06 -0400

    Refactored Project in Correct Location

---
+++ Assets/Player_Scripts/EmpoweredInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpoweredInput : InputPlayer
+{
+    public override InputCollection GetPlayer1Input()
+    {
+        InputCollection IC = InputCollection.GetBlankState();
+        IC.AddAxis("Horizontal", Input.GetAxis("Horizontal"));
+        IC.AddAxis("Vertical", Input.GetAxis("Vertical"));
+        IC.AddButton("Fire1", Input.GetButtonDown("Fire1"));
+        IC.AddButton("Fire2", Input.GetButtonDown("Fire2"));
+        IC.AddButton("Fire3", Input.GetButtonDown("Fire3"));
+        IC.AddButton("Fire4", Input.GetButtonDown("Fire4"));
+        return IC;
+    }
+
+    public override InputCollection GetPlayer2Input()
+    {
+        InputCollection IC = InputCollection.GetBlankState();
+        IC.AddAxis("Horizontal", Input.GetAxis("P2Horizontal"));
+        IC.AddAxis("Vertical", Input.GetAxis("P2Vertical"));
+        IC.AddButton("Fire1", Input.GetButtonDown("P2Fire1"));
+        IC.AddButton("Fire2", Input.GetButtonDown("P2Fire2"));
+        IC.AddButton("Fire3", Input.GetButtonDown("P2Fire3"));
+        IC.AddButton("Fire4", Input.GetButtonDown("P2Fire4"));
+        return IC;
+    }
+
+    public override InputCollection GetPlayer3Input()
+    {
+        InputCollection IC = InputCollection.GetBlankState();
+        IC.AddAxis("Horizontal", Input.GetAxis("P3Horizontal"));
+        IC.AddAxis("Vertical", Input.GetAxis("P3Vertical"));
+        IC.AddButton("Fire1", Input.GetButtonDown("P3Fire1"));
+        IC.AddButton("Fire2", Input.GetButtonDown("P3Fire2"));
+        IC.AddButton("Fire3", Input.GetButtonDown("P3Fire3"));
+        IC.AddButton("Fire4", Input.GetButtonDown("P3Fire4"));
+        return IC;
+    }
+
+    public override InputCollection GetPlayer4Input()
+    {
+        InputCollection IC = InputCollection.GetBlankState();
+        IC.AddAxis("Horizontal", Input.GetAxis("P4Horizontal"));
+        IC.AddAxis("Vertical", Input.GetAxis("P4Vertical"));
+        IC.AddButton("Fire1", Input.GetButtonDown("P4Fire1"));
+        IC.AddButton("Fire2", Input.GetButtonDown("P4Fire2"));
+        IC.AddButton("Fire3", Input.GetButtonDown("P4Fire3"));
+        IC.AddButton("Fire4", Input.GetButtonDown("P4Fire4"));
+        IC.AddButton("Fire5", Input.GetButtonDown("Fire5"));
+        IC.AddButton("Fire6", Input.GetButtonDown("Fire4"));
+        return IC;
+       
+    }
+}
---
+++ Assets/Player_Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public virtual void Player_Base_Stats()
+    {
+        float BaseHP = 500.0f;
+        float BaseSpeed = 10f;
+        float BaseAttackRange = 5f;
+        float BaseArmor = 90f;
+        float BaseAttackSpeed = 15f;
+        //float BaseMana = 100f; We may want to add a base mana bar for spell casters for flavor and more elegant gameplay
+        //float BaseEnergy = 100f; Same for melee and physical classes, it would just look neater, but we can talk before implimentation  
+    }
+    public virtual void SpamAttack()
+    {
+
+    }
+    public virtual void LargeOnCooldownMove()
+    {
+
+    }
+    public virtual void EvasionOrDefensiveMove()
+    {
+
+    }
+}
