467b1b798b33d7e5468a2d6c931c819f8c483f10
Toxic18 <49251120+Toxic18@users.noreply.github.com>
2019-04-22 13:00:21 -0400

    Main Menu Started

---
+++ Assets/Player_Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    string CharacterSelectScene = "CharacterSelection";
+    string TutorialSelectScene = "TutorialScene";
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(CharacterSelectScene);
+    }
+
+    public void PlayTutorial()
+    {
+        SceneManager.LoadScene(TutorialSelectScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+
+    }
+
+
+
+
+
+
+}
