75d7f26c41d18883f5d63cf71d284737f5652b8d
Toxic18 <49251120+Toxic18@users.noreply.github.com>
2019-04-24 18:19:46 -0400

    Main Menu Fixed

---
+++ Assets/SPin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPin : MonoBehaviour
+{
+    public GameObject spin;
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spin.transform.Rotate(Vector3.up);
+    }
+}
